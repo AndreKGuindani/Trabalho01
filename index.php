d707584e1b5cbffd0694130647ddac5d3638dcd6
André Guindani <andreguindaniifc@gmail.com>
2025-05-12 14:51:43 -0300

    final

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-asas
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Iv0t1FVDElLb8v9Z+3PeYc8Wn1u6QmkboCq9+8GkgMxLbKtLwqU0tPQHyoM4IEaN"
+        crossorigin="anonymous"></script>
+    <title>Sites de filmes e séries</title>
+</head>
+
+<body>
+    <header>
+        <nav class="container-sm">
+            <label class="logo">Filmix</label>
+            <ul>
+                <li><a class="active" href="#">Filmes</a></li>
+                <li><a href="#">Series</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+
+    </main>
+
+</body>
+
+</html>
